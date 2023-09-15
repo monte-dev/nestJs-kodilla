cdd4b0fc17efaa3980152d7e32a130e02eb4c93e
monte-dev <badziobw@gmail.com>
2023-09-16 00:41:15 +0100

    add validation with class-validator and class-transformer

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,8 @@
 import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { ProductsService } from './products.service';
-import { db, Product } from '../db';
 import { CreateProductDTO } from './dtos/create-product.dto';
+import { ParseUUIDPipe } from '@nestjs/common';
+
 @Controller('products')
 export class ProductsController {
   constructor(private productsService: ProductsService) {
@@ -12,17 +13,17 @@ export class ProductsController {
   getAll(): any {
     return this.productsService.getAll();
   }
-
   @Get('/:id')
-  public getById(@Param('id') productId: string) {
-    return this.productsService.getById(productId);
+  getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    return this.productsService.getById(id);
   }
 
   @Delete('/:id')
-  public deleteById(@Param('id') productId: string) {
-    this.productsService.deleteById(productId);
+  deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+    this.productsService.deleteById(id);
     return { success: true };
   }
+
   @Post('/')
   create(@Body() productData: CreateProductDTO) {
     return this.productsService.create(productData);
